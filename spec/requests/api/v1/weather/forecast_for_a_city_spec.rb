1d0bd0295393b505d71a5c6f60ca4e5fee6cbb25
Jack Cullen <jpcullen20@gmail.com>
2020-08-01 14:39:31 -0600

    Add test for a different city

--- spec/requests/api/v1/weather/forecast_for_a_city_spec.rb
+++ spec/requests/api/v1/weather/forecast_for_a_city_spec.rb
@@ -15,4 +15,19 @@ describe 'The forcast endpoint' do
     expect(forecast[:data][:attributes]).to have_key(:hourly)
     expect(forecast[:data][:attributes]).to have_key(:daily)
   end
+
+  it 'retuns the weather for a different city' do
+    get '/api/v1/forecast', params: {location: 'San Francisco'}
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    forecast = JSON.parse(response.body, symbolize_names: true)
+
+    expect(forecast[:data][:type]).to eq('forecast')
+    expect(forecast[:data][:attributes][:location]).to eq('San Francisco, CA US')
+    expect(forecast[:data][:attributes]).to have_key(:current)
+    expect(forecast[:data][:attributes]).to have_key(:hourly)
+    expect(forecast[:data][:attributes]).to have_key(:daily)
+  end
 end
