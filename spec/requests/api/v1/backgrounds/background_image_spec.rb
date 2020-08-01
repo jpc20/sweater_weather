c0f6bf871917a738ee57ebd978b982e9f95f83e6
Jack Cullen <jpcullen20@gmail.com>
2020-08-01 15:43:38 -0600

    Add spec for backgound image endpoint

---
+++ spec/requests/api/v1/backgrounds/background_image_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'The backgrounds endpoint' do
+  it 'retuns the url to a background image for a city' do
+    get '/api/v1/backgrounds', params: {location: 'denver,co'}
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    
+  end
+end
