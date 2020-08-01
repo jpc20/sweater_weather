3696b906eb4660495e91c400163b1617f20284f0
Jack Cullen <jpcullen20@gmail.com>
2020-08-01 14:19:43 -0600

    Add open weather results spec

---
+++ spec/poros/open_weather_results_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'OpenWeatherResults PORO' do
+  it 'retuns a forecast object' do
+    location = MapQuestResults.new.get_coordinates('denver.co')
+    forecast = OpenWeatherResults.new.get_forecast(location)
+    expect(forecast.class).to eq(Forecast)
+  end
+end
