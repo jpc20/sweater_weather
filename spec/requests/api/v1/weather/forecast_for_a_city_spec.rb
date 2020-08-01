c9f997abed50bb575ec93a12cab5d4c2560a0d99
Jack Cullen <jpcullen20@gmail.com>
2020-08-01 13:51:44 -0600

    Add expectations to forecast endpoint test

--- spec/requests/api/v1/weather/forecast_for_a_city_spec.rb
+++ spec/requests/api/v1/weather/forecast_for_a_city_spec.rb
@@ -7,5 +7,10 @@ describe 'The forcast endpoint' do
     expect(response).to be_successful
     expect(response.status).to eq(200)
 
+    forecast = JSON.parse(response.body, symbolize_names: true)
+    expect(forecast[:data][:type]).to eq('forecast')
+    expect(forecast[:data][:attributes]).to have_key(:current)
+    expect(forecast[:data][:attributes]).to have_key(:hourly)
+    expect(forecast[:data][:attributes]).to have_key(:daily)
   end
 end
