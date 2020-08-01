a683c4dcba7a82f7a348bc8ad1a433d791b39510
Jack Cullen <jpcullen20@gmail.com>
2020-08-01 13:25:48 -0600

    Add method to format daily forecast

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,9 +1,11 @@
 class Forecast
   attr_reader :current,
-              :hourly
+              :hourly,
+              :daily
   def initialize(weather_data)
     @current = format_current_weather(weather_data)
     @hourly = format_hourly_forecast(weather_data)
+    @daily = format_daily_forecast(weather_data)
   end
 
   private
@@ -20,6 +22,14 @@ class Forecast
   def format_hourly_forecast(weather_data)
     weather_data[:hourly].map do |hourly_data|
       hourly_data.slice(:dt, :temp, :weather)
+    end.first(10)
+  end
+
+  def format_daily_forecast(weather_data)
+    weather_data[:daily].map do |daily_data|
+      daily_data.slice!(:dt, :weather, :rain, :temp)
+      daily_data[:temp].slice!(:min, :max)
+      daily_data
     end.first(10)
   end
 end
