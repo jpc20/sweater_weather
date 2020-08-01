f49df71df333ffa26e2e1c4e0aba1eb517a18f84
Jack Cullen <jpcullen20@gmail.com>
2020-07-31 19:40:32 -0600

    Set up connection to mapquest

--- app/poros/map_quest_results.rb
+++ app/poros/map_quest_results.rb
@@ -1,6 +1,5 @@
 class MapQuestResults
   def get_coordinates(location)
     map_quest_resp = MapQuestService.new.get_coordinates(location)
-    JSON.parse(map_quest_resp)
-  end
+g  end
 end
--- app/services/map_quest_service.rb
+++ app/services/map_quest_service.rb
@@ -1,13 +1,16 @@
 class MapQuestService
 
   def get_coordinates(location)
-  
+    resp = conn.get do |req|
+      req.params['location'] = location
+    end
+    JSON.parse(resp.body)
   end
 
   private
 
   def conn
-    Faraday.new('http://www.mapquestapi.com/directions/v2/route') do |f|
+    Faraday.new('http://www.mapquestapi.com/geocoding/v1/address') do |f|
       f.params['key'] = ENV['MAPQUEST_API_KEY']
     end
   end
