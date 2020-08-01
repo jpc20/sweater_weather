c64020103833b43f9f19bce4ab182fa88e7adecc
Jack Cullen <jpcullen20@gmail.com>
2020-08-01 13:12:20 -0600

    Set up current and hourly methods for forecast

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,11 +1,13 @@
 class Forecast
-  attr_reader :current
+  attr_reader :current,
+              :hourly
   def initialize(weather_data)
-    @current = get_current_weather(weather_data)
+    @current = format_current_weather(weather_data)
+    @hourly = format_hourly_forecast(weather_data)
   end
 
   private
-  def get_current_weather(weather_data)
+  def format_current_weather(weather_data)
     weather_data[:current].without(
                                     :dew_point,
                                     :wind_speed,
@@ -13,5 +15,11 @@ class Forecast
                                     :wind_deg,
                                     :clouds
                                   )
+  end
+
+  def format_hourly_forecast(weather_data)
+    weather_data[:hourly].map do |hourly_data|
+      hourly_data.slice(:dt, :temp, :weather)
+    end.first(10)
   end
 end
