a1a127192c3a400c818f274ff2247fce59a5fa4e
Jack Cullen <jpcullen20@gmail.com>
2020-08-01 14:26:28 -0600

    Add spec for forecast poro

---
+++ spec/poros/forecast_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'Forecast PORO' do
+  it 'has a location, current, hourly, and daily' do
+    location = MapQuestResults.new.get_coordinates('denver,co')
+    forecast = OpenWeatherResults.new.get_forecast(location)
+    expect(forecast.location).to eq('Denver, CO US')
+    expect(forecast.current).to have_key(:weather)
+    expect(forecast.hourly.first).to have_key(:weather)
+    expect(forecast.daily.first).to have_key(:weather)
+  end
+end
--- spec/poros/open_weather_results_spec.rb
+++ spec/poros/open_weather_results_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'OpenWeatherResults PORO' do
   it 'retuns a forecast object' do
-    location = MapQuestResults.new.get_coordinates('denver.co')
+    location = MapQuestResults.new.get_coordinates('denver,co')
     forecast = OpenWeatherResults.new.get_forecast(location)
     expect(forecast.class).to eq(Forecast)
   end
