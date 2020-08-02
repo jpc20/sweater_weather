b3ab61cbb734cb7a7833a81b69d4be03b8e990d8
Jack Cullen <jpcullen20@gmail.com>
2020-08-02 10:39:16 -0600

    Change status code for a successful response

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: UsersSerializer.new(user)
+      render json: UsersSerializer.new(user), status: :created
     end
   end
 
