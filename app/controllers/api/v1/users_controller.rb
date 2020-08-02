57abf622c73d9ac1958e8716f18bbca6eb79b81c
Jack Cullen <jpcullen20@gmail.com>
2020-08-02 10:12:34 -0600

    Add user model

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    user = User.new(user_params)
+  end
 
+  private
+  def user_params
+    params.permit(:email, :password)
   end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates :email, uniqueness: true, presence: true
+  validates :api_key, uniqueness: true, presence: true
+  has_secure_password
+end
