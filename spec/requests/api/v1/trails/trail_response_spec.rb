dccce88b02205fc9a491d1f7262dea0d44ea010d
Jack Cullen <jpcullen20@gmail.com>
2020-08-03 10:18:52 -0600

    Add test for a different city

--- spec/requests/api/v1/trails/trail_response_spec.rb
+++ spec/requests/api/v1/trails/trail_response_spec.rb
@@ -8,9 +8,29 @@ describe 'The trails endpoint' do
     expect(response.status).to eq(200)
 
     trails_resp = JSON.parse(response.body, symbolize_names: true)
-    require "pry"; binding.pry
+
     expect(trails_resp[:data][:type]).to eq('trail')
     expect(trails_resp[:data][:attributes][:location]).to eq('Denver, CO US')
+    expect(trails_resp[:data][:attributes][:forecast]).to have_key(:summary)
+    expect(trails_resp[:data][:attributes][:forecast]).to have_key(:temerature)
+    expect(trails_resp[:data][:attributes][:trails].class).to eq(Array)
+    expect(trails_resp[:data][:attributes][:trails].first).to have_key(:name)
+    expect(trails_resp[:data][:attributes][:trails].first).to have_key(:summary)
+    expect(trails_resp[:data][:attributes][:trails].first).to have_key(:difficulty)
+    expect(trails_resp[:data][:attributes][:trails].first).to have_key(:location)
+    expect(trails_resp[:data][:attributes][:trails].first).to have_key(:distance_to_trail)
+  end
+
+  it 'retuns the forecast  and list of trails for a different city' do
+    get '/api/v1/trails', params: {location: 'san franciscpo'}
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    trails_resp = JSON.parse(response.body, symbolize_names: true)
+
+    expect(trails_resp[:data][:type]).to eq('trail')
+    expect(trails_resp[:data][:attributes][:location]).to eq('San Francisco, CA US')
     expect(trails_resp[:data][:attributes][:forecast]).to have_key(:summary)
     expect(trails_resp[:data][:attributes][:forecast]).to have_key(:temerature)
     expect(trails_resp[:data][:attributes][:trails].class).to eq(Array)
