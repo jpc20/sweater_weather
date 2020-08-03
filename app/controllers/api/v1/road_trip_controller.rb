6d9dbb11d7b9277d4c8d97f85fb1a99713482b60
Jack Cullen <jpcullen20@gmail.com>
2020-08-03 14:48:34 -0600

    Add road trip helper method

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -2,17 +2,20 @@ class Api::V1::RoadTripController < ApplicationController
   def create
     user = User.find_by(api_key)
     if user
-      directions = MapQuestResults.new.directions(from_to)
-      location = MapQuestResults.new.get_coordinates(from_to[:destination])
-      forecast = OpenWeatherResults.new.get_forecast(location)
-      road_trip = RoadTrip.new(from_to, directions, forecast)
-      render json: RoadTripSerializer.new(road_trip)
+      render json: RoadTripSerializer.new(road_trip(from_to))
     else
       render json: ErrorSerializer.new.unauthorized_error, status: :unauthorized
     end
   end
 
   private
+
+  def road_trip(from_to)
+    directions = MapQuestResults.new.directions(from_to)
+    location = MapQuestResults.new.get_coordinates(from_to[:destination])
+    forecast = OpenWeatherResults.new.get_forecast(location)
+    RoadTrip.new(from_to, directions, forecast)
+  end
 
   def from_to
     JSON.parse(request.body.read, symbolize_names: true).without(:api_key)
