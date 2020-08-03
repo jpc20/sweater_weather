7d4cd20c3135b48d77f9ffb9e1dcb5612e75b0c2
Jack Cullen <jpcullen20@gmail.com>
2020-08-03 11:30:25 -0600

    Add helper method to get the trails guide object

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -2,12 +2,17 @@ class Api::V1::TrailsController < ApplicationController
   def index
     location = MapQuestResults.new.get_coordinates(params[:location])
     if location.name.split(',')[0] != ''
-      forecast = OpenWeatherResults.new.get_forecast(location)
-      trails = HikingProjectResults.new.get_trails(location)
-      trails_guide = TrailsGuide.new(forecast,trails)
-      render json: TrailsGuideSerializer.new(trails_guide)
+      render json: TrailsGuideSerializer.new(trails_guide(location))
     else
       render json: ErrorSerializer.new.location_error, status: :bad_request
     end
+  end
+
+  private
+
+  def trails_guide(location)
+    forecast = OpenWeatherResults.new.get_forecast(location)
+    trails = HikingProjectResults.new.get_trails(location)
+    TrailsGuide.new(forecast,trails)
   end
 end
