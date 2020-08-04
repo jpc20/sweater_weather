9ab03f8d81465829e79503ab664e04ce6d70dad0
Jack Cullen <jpcullen20@gmail.com>
2020-08-04 09:11:10 -0600

    Add expectations

--- spec/requests/api/v1/backgrounds/background_image_spec.rb
+++ spec/requests/api/v1/backgrounds/background_image_spec.rb
@@ -6,11 +6,21 @@ describe 'The backgrounds endpoint' do
 
     expect(response).to be_successful
     expect(response.status).to eq(200)
+
+    image = JSON.parse(response.body, symbolize_names: true)
+
+    expect(image[:data][:type]).to eq('image')
+    expect(image[:data][:attributes]).to have_key(:url)
   end
   it 'retuns the url to a background image for a differentcity', :vcr do
     get '/api/v1/backgrounds', params: {location: 'san francisco, ca'}
 
     expect(response).to be_successful
     expect(response.status).to eq(200)
+
+    image = JSON.parse(response.body, symbolize_names: true)
+
+    expect(image[:data][:type]).to eq('image')
+    expect(image[:data][:attributes]).to have_key(:url)
   end
 end
